7d865c64adce74fc0ab6727596d4fe37578bfc35
nabtron <developer@nabtron.com>
2016-12-03 23:55:55 +0500

    Update clearboth.php

--- clearboth.php
+++ clearboth.php
@@ -3,16 +3,16 @@
 Plugin Name: ClearBoth
 Plugin URI: http://www.nabtron.com/clearboth/
 Description: Add clear both via line break or div using shortcode: <strong>[clearboth]</strong> in your post or page plugin for wordpress
-Version: 1.0.1
+Version: 1.0.3
 Author: Nabtron
 Author URI: http://nabtron.com/
 Min WP Version: 4.2
-Max WP Version: 4.5
+Max WP Version: 4.7
 */
 
 class nabclearboth_main {
 	public static function nabclearboth_func( $atts ) {
-		return '<div style="clear:both" />';
+		return '<br style="clear:both" />';
 	}
  }
  add_shortcode( 'clearboth', array( 'nabclearboth_main', 'nabclearboth_func' ) );
