3ecaa241c7d66457d520846dd270e04ea37d0325
nabtron <developer@nabtron.com>
2021-10-29 05:22:19 +0500

    1.0.3.6

--- clearboth.php
+++ clearboth.php
@@ -1,18 +1,20 @@
 <?php
 /*
 Plugin Name: ClearBoth
-Plugin URI: http://www.nabtron.com/clearboth/
-Description: Add linebreak with css style <strong>clear:both</strong> from wordpress editor using shortcode: <strong>[clearboth]</strong> in your post or page plugin for wordpress
-Version: 1.0.3.2
+Plugin URI: https://nabtron.com/clearboth/
+Description: Add linebreak with css style <strong>clear:both</strong> from wordpress editor using shortcode: <strong>[clearboth]</strong> in your post or page.
+Version: 1.0.3.6
 Author: Nabtron
-Author URI: http://nabtron.com/
+Author URI: https://nabtron.com/
 Min WP Version: 4.2
-Max WP Version: 5.0
+Max WP Version: 5.8.1
 */
 
-class nabclearboth_main {
-	public static function nabclearboth_func( $atts ) {
+class nabclearboth_main
+{
+	public static function nabclearboth_func($atts)
+	{
 		return '<br style="clear:both" />';
 	}
- }
- add_shortcode( 'clearboth', array( 'nabclearboth_main', 'nabclearboth_func' ) );
+}
+add_shortcode('clearboth', array('nabclearboth_main', 'nabclearboth_func'));
